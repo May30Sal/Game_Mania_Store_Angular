c9345ce2bbdadc90d12dede66585fbb3c722e79c
May30Sal <maysaldanha@hotmail.com>
2021-11-10 15:40:25 -0300

    Implementação do segundo carousel, da página 'Sobre' e responsividade

--- src/app/pastas/home/home.component.ts
+++ src/app/pastas/home/home.component.ts
@@ -15,9 +15,6 @@ export class HomeComponent implements OnInit {
     loop: true,
     autoplay: true,
     stagePadding: 7,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
     dots: false,
     margin: 20,
     animateOut: 'fadeOut',
@@ -39,6 +36,40 @@ export class HomeComponent implements OnInit {
       },
       1000: {
         items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  };
+
+  customOptions2: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    stagePadding: 7,
+    dots: false,
+    autoWidth: true,
+    margin: 20,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    items: 2,
+    navText: [
+      "<img src='./assets/imagens/icon_previous.png'>",
+      "<img src='./assets/imagens/icon_next.png'>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        autoWidth: false,
+      },
+      600: {
+        items: 1,
+        nav: false,
+        autoWidth: false,
+      },
+      1000: {
+        items: 2,
         nav: true,
         loop: true,
       },
